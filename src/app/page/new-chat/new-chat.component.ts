48f64e81a6ce984e0d93d41e11c66a6808118cf5
Matt Costigan <costigan@uchicago.edu>
2023-07-14 11:48:08 -0500

    create new chats

--- src/app/page/new-chat/new-chat.component.ts
+++ src/app/page/new-chat/new-chat.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {ChatService} from "../../service/chat/chat.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-new-chat',
@@ -11,14 +12,18 @@ export class NewChatComponent implements OnInit {
   description: string | null = null
   user: string = ''
 
-  constructor(private chatService: ChatService) {
+  constructor(private chatService: ChatService, private router: Router) {
   }
 
   ngOnInit(): void {
   }
 
   create() {
-    this.chatService.createChat(this.name, this.description, [this.user]).subscribe()
+    this.chatService.createChat(this.name, this.description, this.user.split(",")).subscribe(
+      ()=>{
+        void this.router.navigateByUrl('')
+      }
+    )
   }
 
 }
